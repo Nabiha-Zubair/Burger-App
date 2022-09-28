8684c73dc7a184f59c5a79a38d894857d269970d
Nabiha Zubair <nabiha.zubair@devsinc.com>
2022-09-28 20:35:35 +0500

    Dev: PR comments resolved

--- src/components/Burger/burger.jsx
+++ src/components/Burger/burger.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import './burger.css'
 import { Buttons } from './buttons'
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import AuthContext from '../Authentication/auth-context'
 import ButtonsContext from './buttons-context'
 
@@ -11,67 +11,40 @@ export const Burger = () => {
   const { auth } = useContext(AuthContext)
   const navigate = useNavigate()
   const [price, setPrice] = useState(3)
-  const [ingredients, setIngredients] = useState({ lettuce: 0, bacon: 0, cheese: 0, meat: 0 })
-  const [length, setLength] = useState(-1)
-  useEffect(() => {
-    if (ingredients.lettuce == 0 && ingredients.bacon == 0 && ingredients.cheese == 0 && ingredients.meat == 0) {
-      setLength(0)
-    }
-    console.log("length:", length)
-
-  }, [setIngredients])
+  const [ingredients, setIngredients] = useState({ lettuce: [], bacon: [], cheese: [], meat: [] })
+  const prices = { lettuce: 0.70, bacon: 0.30, cheese: 0.40, meat: 1.40 }
 
   const updateIngredients = (action, label) => {
-    let quantity = 0, ingreditent_price = 0;
-    action == 'add' ? quantity += 1 : quantity -= 1
-
-    switch (label) {
-      case 'lettuce':
-        quantity += ingredients.lettuce
-        quantity = quantity < 0 ? 0 : quantity
-        ingreditent_price = 0.70
-        setIngredients({ ...ingredients, lettuce: quantity })
-        break;
-      case 'cheese':
-        quantity += ingredients.cheese
-        quantity = quantity < 0 ? 0 : quantity
-        ingreditent_price = 0.40
-        setIngredients({ ...ingredients, cheese: quantity })
-        break;
-      case 'bacon':
-        quantity += ingredients.bacon
-        quantity = quantity < 0 ? 0 : quantity
-        ingreditent_price = 0.30
-        setIngredients({ ...ingredients, bacon: quantity })
-        break;
-      case 'meat':
-        quantity += ingredients.meat
-        quantity = quantity < 0 ? 0 : quantity
-        ingreditent_price = 1.40
-        setIngredients({ ...ingredients, meat: quantity })
-        break;
-    }
-    action == 'add' ? setPrice(price + ingreditent_price) : setPrice(price - ingreditent_price)
+    let quantity = 0;
+    quantity = ingredients[label]
+    action === 'add' ? quantity.push(label) : quantity.pop()
+    setIngredients({ ...ingredients, label: quantity })
+    action === 'add' ? setPrice(price + prices[label]) : setPrice(price - prices[label])
   }
   const BurgerItems = () => {
 
     let burger = [];
-    for (let i = 0; i < ingredients.lettuce; i++) {
+    ingredients.lettuce.forEach((item, key) => {
       burger.push(<div key={burger.length} className="lettuse"></div>);
     }
-    for (let i = 0; i < ingredients.bacon; i++) {
+    )
+
+    ingredients.cheese.forEach(
+      (item, key) => {
+        burger.push(<div key={burger.length} className="cheese"></div>);
+      }
+    )
+
+    ingredients.bacon.forEach((item, key) => {
       burger.push(<div key={burger.length} className="bacon"></div>);
     }
-    for (let i = 0; i < ingredients.cheese; i++) {
-      burger.push(<div key={burger.length} className="cheese"></div>);
-    }
-    for (let i = 0; i < ingredients.meat; i++) {
+    )
+
+    ingredients.meat.forEach((item, key) => {
       burger.push(<div key={burger.length} className="meat"></div>);
     }
+    )
 
-    if (burger.length == 0) {
-      burger.push(<h4>No Ingredients Added</h4>)
-    }
     return burger
   }
 
@@ -79,24 +52,28 @@ export const Burger = () => {
     <>
       <div className="burger-container mt-5">
         <div className="burger-top"></div>
-        <BurgerItems />
+        {
+          BurgerItems().length !== 0 ?
+            <BurgerItems />
+            : <h4>No Ingredients Added</h4>
+        }
         <div className="burger-bottom"></div>
       </div>
       <div className="options-container" key={ingredients.length}>
-        <ButtonsContext.Provider value={{ ingredients, updateIngredients }}>
+        <ButtonsContext.Provider value={{ setIngredients, updateIngredients }}>
 
           <p>Current price: <span> ${price.toFixed(2)}</span></p>
-          <Buttons label="lettuce" quantity={ingredients.lettuce} />
-          <Buttons label="cheese" quantity={ingredients.cheese} />
-          <Buttons label="bacon" quantity={ingredients.bacon} />
-          <Buttons label="meat" quantity={ingredients.meat} />
+          <Buttons label={"lettuce"} quantity={ingredients.lettuce.length} />
+          <Buttons label={"cheese"} quantity={ingredients.cheese.length} />
+          <Buttons label="bacon" quantity={ingredients.bacon.length} />
+          <Buttons label="meat" quantity={ingredients.meat.length} />
 
         </ButtonsContext.Provider>
-        {console.log("len: ", BurgerItems().length)}
+
         {auth.currentUser ?
-          <button className={BurgerItems().length == 1 ? 'disbale-btn checkout' : 'checkout-btn'} disabled={BurgerItems().length == 1} onClick={() => navigate('/order')}>CHECKOUT</button>
+          <button className={BurgerItems().length === 0 ? 'btn btn-secondary checkout' : 'btn btn-primary'} disabled={BurgerItems().length === 0} onClick={() => navigate('/order')}>CHECKOUT</button>
           :
-          <button className={BurgerItems().length == 1 ? 'disbale-btn checkout' : 'checkout-btn'} disabled={BurgerItems().length == 1} onClick={() => navigate('/auth')}>Sign up to checkout</button>
+          <button className={BurgerItems().length === 0 ? 'btn btn-secondary checkout' : 'btn btn-primary'} disabled={BurgerItems().length === 0} onClick={() => navigate('/auth')}>Sign up to checkout</button>
 
         }
 
--- src/components/Burger/buttons.jsx
+++ src/components/Burger/buttons.jsx
@@ -3,17 +3,17 @@ import ButtonsContext from './buttons-context'
 import './burger.css';
 
 export const Buttons = (props) => {
-  const { ingredients, updateIngredients } = useContext(ButtonsContext)
+  const { updateIngredients } = useContext(ButtonsContext)
 
   return (
     <div className='container'>
       <div className='buttons-rows'>
         <p>{props.label}</p>
         <div className="btns mt-2">
-          <button className={props.quantity == 0 ? 'disbale-btn' : 'decrease-btn'} onClick={() => updateIngredients('minus', props.label)} disabled={props.quantity == 0}> Less </button>
+          <button className={props.quantity === 0 ? 'btn btn-secondary' : 'btn btn-success decrease-btn'} onClick={() => updateIngredients('minus', props.label)} disabled={props.quantity === 0}> Less </button>
           <button className='increase-btn' onClick={() => updateIngredients('add', props.label)}> More </button>
         </div>
       </div>
-    </div >
+    </div>
   )
 }
